b84b2111c69c643a0874a37b8546a687372c9027
VishuKalier2003 <vishukalier123@gmail.com>
2025-05-28 19:26:37 +0530

    facade pattern update

--- Design/Facade/SimpleFacade.java
+++ Design/Facade/SimpleFacade.java
@@ -1,23 +1,24 @@
-package Facade;
+package Facade; // Facade pattern is the technique of Loose coupling
 
 // Provides a simplified interface for the client to work on, facade interacts with the subsystem classes
+@SuppressWarnings("unused")
 public class SimpleFacade { // The client never calls the subsystem classes grouped by facade
 
     // The main business code needs only 3 facade methods instead of multiple
     // subsystem methods
-    public int operation1(int x) { // Class 1 function
+    private int operation1(int x) { // Class 1 function
         Operation1 op1 = new Operation1();
         return op1.operation1(x);
     }
 
     // The proxy pattern can be woven into the facade, rather than each subsystem
-    public double operation2(int x) { // Class 2 function
+    private double operation2(int x) { // Class 2 function
         Operation1 op1 = new Operation1();
         Operation2 op2 = new Operation2();
         return op2.operation2(op1, x);
     }
 
-    public double operation3(int x) { // Class 3 function
+    private double operation3(int x) { // Class 3 function
         Operation1 op1 = new Operation1();
         Operation2 op2 = new Operation2();
         Operation3 op3 = new Operation3();

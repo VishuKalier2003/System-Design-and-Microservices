547e271c4d9c9f177ea304a17d7431a2fbc5cd2a
VishuKalier2003 <vishukalier123@gmail.com>
2025-05-29 08:33:19 +0530

    adapter pattern started

---
+++ Design/Adapter/ThreeVolt.java
@@ -0,0 +1,7 @@
+
+
+// Adapter interface for 3 volt
+public interface ThreeVolt {
+    // Uses the same functions as that of adaptee class (which needs to be changed as per use)
+    public int getCurrent();
+}
